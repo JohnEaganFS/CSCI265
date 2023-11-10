d81731ceee648f6279662a44bbfbeb3571815289
John Eagan <johnjeagan@mail.fresnostate.edu>
2023-11-10 06:30:43 -0800

    fixing

--- Cycling/scripts/test_model.py
+++ Cycling/scripts/test_model.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     # Load agent model
     # model = PPO.load("../models/temp_model")
     # model = PPO.load("../eval_models/best_model.zip")
-    model = PPO.load("../eval_models/actually_the_best.zip")
+    model = PPO.load("../eval_models/actually_the_best.zip", env=env, custom_objects={'observation_space': env.observation_space, 'action_space': env.action_space})
 
 
     # Play episodes
